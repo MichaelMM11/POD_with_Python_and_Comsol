7646cc0cd5c09876c6e1644785a11ae4a00b6cee
MichaelMM11 <mich4y@googlemail.com>
2023-05-25 00:24:28 +0200

    setup basic structure (1) created MWE .mph file for trial&error (2) created .py file that interacts with .mph file (3) created default repo structure

---
+++ src/model_interaction.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+purpose
+    - interact with .mph files with .py script
+      - modify .mph model
+      - get stiffness matrix, vectors...
+
+remark
+    - be connected to university internet because of Comsol instance
+    - https://mph.readthedocs.io/en/stable/tutorial.html
+"""
+
+import mph
+from pathlib import Path
+
+
+
+current__dir = Path.cwd()
+main__dir = current__dir.parents[0]
+comsol__dir = main__dir.joinpath('comsol')
+
+comsol_filename = 'Heat_Equation.mph'
+
+file_to_load = Path(comsol__dir, comsol_filename)
+
+
+
+client = mph.start()
+model = client.load(file_to_load)
+print(f"{client.names()}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("Hello, World!")
